6f9c11eab194dfe991f3b2cd38e1127d3208212f
Jarrod <jarrod.skulavik@outlook.com>
2015-04-13 00:34:37 +0000

    Initial commit for basic console app with assembly load errors

---
+++ mapconsole/mapconsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using NETMapnik;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mapconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VectorTile vt = new VectorTile(1, 0, 0);
+            vt.AddGeoJSON("{}", "layer0");
+        }
+    }
+}
